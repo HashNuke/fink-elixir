157524a7444983c48dd31116cf59a6a030cc9eab
Roman Gladkov <d1fffuz0r@gmail.com>
2015-01-12 23:09:47 -0800

    update

--- lib/exfink.ex
+++ lib/exfink.ex
@@ -1,38 +1,11 @@
-defmodule ExFink do
-
-  def add_lager_backend do
-    :fink.add_lager_backend
-  end
-
-  def add_sasl_backend do
-    :fink.add_sasl_backend
-  end
-
-  def add_sasl_backend(args) do
-    :fink.add_sasl_backend args
-  end
-
-  def fcatch(fun) do
-    :fink.fcatch fun
-  end
-
-  def fcatch(fun, onerror) do
-    :fink.fcatch fun, onerror
-  end
-
-  def stacktrce(error, reason) do
-    :fink.stacktrace error, reason
-  end
-
-  def stacktrce(error, reason, meta) do
-    :fink.stacktrace error, reason, meta
-  end
-
-  def push(message) do
-    :fink.push message
-  end
-
-  def push(message, params) do
-    :fink.push message, params
-  end
+defmodule ExFink.Main do
+  def add_lager_backend,              do: :fink.add_lager_backend
+  def add_sasl_backend,               do: :fink.add_sasl_backend
+  def add_sasl_backend(args),         do: :fink.add_sasl_backend(args)
+  def fcatch(fun),                    do: :fink.fcatch(fun)
+  def fcatch(fun, onerror),           do: :fink.fcatch(fun, onerror)
+  def stacktrce(error, reason),       do: :fink.stacktrace(error, reason)
+  def stacktrce(error, reason, meta), do: :fink.stacktrace(error, reason, meta)
+  def push(message),                  do: :fink.push(message)
+  def push(message, params),          do: :fink.push(message, params)
 end
--- lib/exfink/exfink_plug.ex
+++ lib/exfink/exfink_plug.ex
@@ -1,10 +1,26 @@
 defmodule ExFink.Plug do
+
+  def get_request_info(conn) do
+    [
+        {"path", ""},
+        {"method", ""},
+        {"args_get", ""},
+        {"args_post", ""},
+        {"module", ""},
+        {"session", ""},
+        {"peer", ""},
+        {"headers", ""}
+    ]
+  end
+
   def stacktrace(kind, reason, stack) do
     :io.format "ERROR - ~p - ~p - ~p", [kind, reason, stack]
     []
   end
 
-  def handle_error(_conn, %{kind: kind, reason: reason, stack: _stack}) do
+  def handle_error(conn, %{kind: kind, reason: reason, stack: _stack}) do
+    request = @get_request_info conn
+    meta = [{"request", request}]
     :io.format "ERROR - ~p - ~p", [kind, reason]
     # ExFink.stacktrace kind, reason
   end
