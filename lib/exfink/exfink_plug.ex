15e35d83772e3f29d830ffcfb8fc5d6204693f2b
Roman Gladkov <d1fffuz0r@gmail.com>
2015-01-12 23:10:41 -0800

    fix

--- lib/exfink/exfink_plug.ex
+++ lib/exfink/exfink_plug.ex
@@ -19,7 +19,7 @@ defmodule ExFink.Plug do
   end
 
   def handle_error(conn, %{kind: kind, reason: reason, stack: _stack}) do
-    request = @get_request_info conn
+    request = ExFink.Plug.get_request_info(conn)
     meta = [{"request", request}]
     :io.format "ERROR - ~p - ~p", [kind, reason]
     # ExFink.stacktrace kind, reason
